bbd1aac627049d51fcc6b194c43c988a5368f5d9
AtlasLioness <narjisaliane@gmail.com>
2024-05-01 19:50:06 +0000

    correcting console, console unittest, base model, mysql dev and storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {"BaseModel": BaseModel, "User": User, "Place": Place,
+           "State": State, "City": City, "Amenity": Amenity,
+           "Review": Review}
 
 
 class FileStorage:
@@ -8,43 +19,48 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            new_dict = {}
+            for k, v in self.__objects.items():
+                if cls == v.__class__ or cls == v.__class__.__name__:
+                    new_dict[k] = v
+            return new_dict
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        if obj is not None:
+            k = obj.__class__.__name__ + "." + obj.id
+            self.__objects[k] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        json_objects = {}
+        for k in self.__objects:
+            json_objects[k] = self.__objects[k].to_dict()
+
+        with open(self.__file_path, 'w') as f:
+            json.dump(json_objects, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
+            with open(self.__file_path, 'r') as f:
+                re = json.load(f)
+            for k in re:
+                self.__objects[k] = classes[re[k]['__class__']](**re[k])
+        except:
             pass
+
+    def delete(self, obj=None):
+        """delets obj from __objects"""
+        if obj is not None:
+            k = obj.__class__.__name__ + '.' + obj.id
+            if k in self.__objects:
+                del self.__objects[k]
+
+    def close(self):
+        """calls reload() for JSON deserialization"""
+        self.reload()
---
+++ tests/test_console.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+TestConsoleDocs
+"""
+
+import console
+import inspect
+import pep8
+import unittest
+HBNBCommand = console.HBNBCommand
+
+
+class TestConsoleDocs(unittest.TestCase):
+    """Class to test console docs"""
+    def test_pep8_conformance_console(self):
+        """Test console.py PEP8 conformity"""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_test_console(self):
+        """Test test/test_console.py PEP8 conformity"""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8.check_files(['test/test_console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_console_module_docstring(self):
+        """Test console.py module docstring"""
+        self.assertIsNot(console.__doc__, None,
+                         "console.py needs a docstring")
+        self.assertTrue(len(console.__doc__) >= 1,
+                        "console.py needs a docstring")
+
+    def test_HBNBCommand_class_docstring(self):
+        """Test HBNBCommand docstring"""
+        self.assertIsNot(HBNBCommand.__doc__, None,
+                         "HBNBCommand class needs a docstring")
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
+                        "HBNBCommand class needs a docstring")
