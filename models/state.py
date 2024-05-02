7b8015b358bc4e4b9780adec8f068a588877c468
Naouwa <densali.nahla@gmail.com>
2024-05-02 01:32:06 +0100

    models

--- models/state.py
+++ models/state.py
@@ -1,8 +1,33 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+import sqlalchemy
+from os import getenv
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State representation"""
+    if models.storage_t == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """State initialization"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """The list of city instances related to the state's getter"""
+            cityList = []
+            allCities = models.storage.all(City)
+            for city in allCities.value():
+                if city.state_id == self.id:
+                    cityList.append(city)
+            return cityList
--- models/user.py
+++ models/user.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    if models.storage_t == "db":
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        passwrd = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
+
+    def __init__(self, *args, **kwargs):
+        """Initializing a user"""
+        super().__init__(*args, **kwargs)
