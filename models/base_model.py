fcf2de9942748ad7181b50bd69291ee8edd9b754
AtlasLioness <narjisaliane@gmail.com>
2024-05-02 14:40:18 +0000

    dbstorage check

--- models/base_model.py
+++ models/base_model.py
@@ -42,10 +42,11 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = self.created_at
-            
+
     def __str__(self):
         """Returns a string representation of the instance"""
-        return '[{:s}] ({:s}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
+        return '[{:s}] ({:s}) {}'.format(self.__class__.__name__,
+                                         self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
